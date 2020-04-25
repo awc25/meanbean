bd2be37bc01a995b161a050ad7ad8c7fc8fa68b2
EEMartin2008 <64328151+EEMartin2008@users.noreply.github.com>
2020-04-25 17:28:01 -0400

    I did the user input homework

--- userinput.py
+++ userinput.py
@@ -1,11 +1,18 @@
 # You can use the input() command to allow the user to type stuff in.
 # It returns a string.
 # This will ask you 3 questions and then print out some things.
-name = input("what's your name? ")
-stories = input("what are your stories? ")
-game = input("what is your favourite game? ")
-print("---done with questions---")
-print("oh cool, %s, I like %s too" % (name, game))
-print("nice stories about %s" % (stories))
+# name = input("what's your name? ")
+# stories = input("what are your stories? ")
+# game = input("what is your favourite game? ")
+# print("---done with questions---")
+# print("oh cool, %s, I like %s too" % (name, game))
+# print("nice stories about %s" % (stories))
 
 # TODO: ask the user some questions of your own and print out responses of your own.
+name = input("what's your name? ")
+Food = input("what is your favorite food? ")
+Pets = input("would you ever like a pet, if so what? ")
+print("---done with questions---")
+print("oh cool, %s, I don't like %s" % (name, Food ))
+print("I like your choice of a %s)" % (Pets))
+
