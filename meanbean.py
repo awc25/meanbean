d4bd3da8251d60ee5ec1b0d6a6c6dfc3364638c4
EEMartin2008 <64328151+EEMartin2008@users.noreply.github.com>
2020-04-25 16:56:48 -0400

    I did my homework

--- meanbean.py
+++ meanbean.py
@@ -15,6 +15,9 @@ print("there are %d rows" % (num_rows))
 print("there are %d rows and %d columns" % (num_rows, num_columns))
 
 # TODO: find the total number of cells in the grid
+num_cells = num_rows * num_columns
+print("The total number of cells is... %d" % (num_cells))
+
 # use * for multiplication
 # assign the result to a new variable, num_cells
 # then print this out with a nice message
@@ -23,13 +26,25 @@ print("there are %d rows and %d columns" % (num_rows, num_columns))
 # you can use %s to print a string:
 firstname = "Ren"
 lastname = "Stimpy"
+
 print("the names are %s%s" % (firstname, lastname))
 # notice how this squishes together the two names -> RenStimpy
 # because there was no space between the %s and the other %s
 
 # TODO: store your first and last names as variables
+firstname = "Emily"
+lastname = "Martin"
+print("My name is %s %s" % (firstname, lastname))
+print("My name is %s, %s" % (lastname, firstname))
+
 # then print your name in two different ways,
 # one with last name first and the other with first name first
 # use fake names and disguise your voice
 
 # hi Emily is here
+firstname = "Lyra"
+lastname = "Martin"
+print("My name is %s %s" % (firstname, lastname))
+print ("%s is the best cat that the %s's ever owned" % (firstname, lastname))
+print ("the %s's the best owner that %s's ever ruled over" % (lastname, firstname))
+print ("%s is the queen of the house" % (firstname))
