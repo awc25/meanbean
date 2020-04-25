40b5171693d167ff2bd7e901b23f445d1135d11d
awc25 <64295756+awc25@users.noreply.github.com>
2020-04-24 23:46:51 -0400

    Create meanbean.py

---
+++ meanbean.py
@@ -0,0 +1,19 @@
+# anything starting with the pound-sign is a comment
+
+# if you want to print out a string, it must be in quotes
+print("beans, beans, beans")
+
+# define variables with single equal sign
+num_rows = 20
+num_columns = 8
+
+# the %d is used to print a number
+print("there are %d rows" % (num_rows))
+
+# there are two %d - therefore, you need two numbers
+print("there are %d rows and %d columns" % (num_rows, num_columns))
+
+# TODO: find the total number of cells in the grid
+# use * for multiplication
+# assign the result to a new variable, num_cells
+# then print this out with a nice message
