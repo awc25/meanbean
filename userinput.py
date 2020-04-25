5bb6078d3122ad5bee269cf445a616d1c78627b7
awc25 <64295756+awc25@users.noreply.github.com>
2020-04-25 15:46:05 -0400

    added something from terminal

---
+++ userinput.py
@@ -0,0 +1,11 @@
+# You can use the input() command to allow the user to type stuff in.
+# It returns a string.
+# This will ask you 3 questions and then print out some things.
+name = input("what's your name? ")
+stories = input("what are your stories? ")
+game = input("what is your favourite game? ")
+print("---done with questions---")
+print("oh cool, %s, I like %s too" % (name, game))
+print("nice stories about %s" % (stories))
+
+# TODO: ask the user some questions of your own and print out responses of your own.
